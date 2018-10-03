317841b9b40025f5ec4620dc44e83ac714f4833c
dmanikym <dmanikyam@traveltripper.com>
2018-10-03 16:00:30 +0530

    Contact form validations with focus to error field implemented

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -139,3 +139,26 @@ $(".banner-fullscreen .primary-btn").on('click', function (e) {
   });
 
 });
+
+
+//Contact Form Validations
+(function () {
+  'use strict';
+  window.addEventListener('load', function () {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function (form) {
+      form.addEventListener('submit', function (event) {
+        if (form.checkValidity() === false) {
+          $('.needs-validation').find('.form-control:invalid').focus(function () {
+            $(window).scrollTop($(this).offset().top - 100);
+          });
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
